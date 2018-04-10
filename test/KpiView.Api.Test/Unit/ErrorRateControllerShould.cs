893ae07e65ad9b0827b15fc2a72fa3b11d5af402
Patrick Kelly <pkelly14159@hotmail.com>
2018-04-10 06:23:51 -0400

    Failing test bookmark. ErrorRateController does not yet distinguish between success and failure cases

--- test/KpiView.Api.Test/Unit/ErrorRateControllerShould.cs
+++ test/KpiView.Api.Test/Unit/ErrorRateControllerShould.cs
@@ -2,23 +2,45 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
-namespace KpiView.Api.Test.Unit {
-    public class ErrorRateControllerShould {
+namespace KpiView.Api.Test.Unit
+{
+    public class ErrorRateControllerShould
+    {
+        private KpiDbContext context;
+        private ErrorRateController controller;
+
+        public ErrorRateControllerShould()
+        {
+            var options = new DbContextOptionsBuilder<KpiDbContext>()
+                .UseInMemoryDatabase("kpi").Options;
+            context = new KpiDbContext(options);
+            controller = new ErrorRateController(context);
+
+        }
 
         [Fact]
-        public void Return100PercentWhenOnlyOneFailedRecordExists () 
+        public void Return100PercentWhenOnlyOneFailedRecordExists()
         {
-            var options = new DbContextOptionsBuilder<KpiDbContext> ()
-                .UseInMemoryDatabase ("kpi").Options;
-            var context = new KpiDbContext (options);
-            context.CallOutcomes.Add (
-                new CallOutcome { Id = 1, Timestamp = DateTime.Now.AddSeconds (-1.0), IsError = true });
+            context.CallOutcomes.Add(
+                new CallOutcome { Id = 1, Timestamp = DateTime.Now.AddSeconds(-1.0), IsError = true });
             context.SaveChanges();
-            var controller = new ErrorRateController (context);
 
-            var result = controller.Get ();
+            var result = controller.Get();
 
             Assert.Equal(1.0M, result.Rate);
+        }
+
+        [Fact]
+        public void ReturnZeroWhenOnlyOneSuccessRecordExists()
+        {
+            context.CallOutcomes.Add(
+                new CallOutcome {Id = 2, Timestamp = DateTime.Now.AddSeconds(-1.0), IsError = false});
+            context.SaveChanges();
+
+            var result = controller.Get();
+
+            Assert.Equal(0.0M, result.Rate);
+            
         }
     }
 }
